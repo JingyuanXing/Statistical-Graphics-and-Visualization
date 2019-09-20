8a07e1a40ed4f88d37268ca7069f15811a7b5fa7
JingyuanXing <lucyxing0217@gmail.com>
2019-09-20 02:01:24 -0400

    a couple of notes to files in this folder

---
+++ README.R
@@ -0,0 +1,3 @@
+print("All files are written in R Markdown")
+print("Data Visualizations can be viewed by knitting the .Rmd files using Rstudio")
+print("Graphics are mainly done using ggplot, from the tidyverse package")
