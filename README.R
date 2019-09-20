1e5e9e6a7912cd5c4cae6e028669c463453579e9
JingyuanXing <lucyxing0217@gmail.com>
2019-09-20 02:08:13 -0400

    Update README.R

--- README.R
+++ README.R
@@ -3,7 +3,8 @@ readme <- function(n){
   print("All files are written in R Markdown")
   print("Data Visualizations can be viewed by knitting the .Rmd files using Rstudio")
   print("Graphics are mainly done using ggplot, from the tidyverse package")
-  print("and, hello world", "!" * n)
+  print("and, hello world!")
+  return n * 42
 }
 
-readme(3)
+readme(1)
