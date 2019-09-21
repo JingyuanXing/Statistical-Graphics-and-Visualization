555201549022e9a8eb8a640319378aa18d120fac
JingyuanXing <lucyxing0217@gmail.com>
2019-09-21 12:47:20 -0400

    Add files via upload

---
+++ DataVisualizationREADME.R
@@ -0,0 +1,11 @@
+
+readme <- function(n){
+    print("All files are written in R Markdown")
+    print("Graphics are mainly done using ggplot, from the tidyverse package")
+    print("Data Visualizations can be viewed by knitting the .Rmd files using Rstudio")
+    print("You are able to view the data graphics in pdf or html form")
+    print("and, hello world!")
+    return (n * 42)
+    }
+
+readme(1)
